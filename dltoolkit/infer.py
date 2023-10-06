6629c4341da17b61b494e8b97e5478cbd359e0f0
Artyom Kharinaev <akharinaev@gmail.com>
2023-10-06 22:51:41 +0300

    Project infrastructure

---
+++ dltoolkit/infer.py
@@ -0,0 +1,6 @@
+import time
+
+
+if __name__ == "main":
+    time.sleep(1)
+    print("Inference done")
---
+++ dltoolkit/train.py
@@ -0,0 +1,12 @@
+# import torch
+# from torch import nn
+import time
+
+
+def some_bad_func(abs: str) -> str:
+    return abs + " "
+
+
+if __name__ == "main":
+    time.sleep(2)
+    print("Training done")
